30947b780b6b0dc21c07e74e0717296523175876
emmanuel <emmanuel.quist@mnsu.edu>
2022-01-01 21:58:26 -0600

    enhanced api documentation

--- src/main/java/com/myretail/productsapi/controller/ProductController.java
+++ src/main/java/com/myretail/productsapi/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.myretail.productsapi.controller;
 
 import com.myretail.productsapi.domain.Product;
 import com.myretail.productsapi.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +29,12 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update price for this product. Admin access required.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Price updated"),
+            @ApiResponse(responseCode = "400", description = "productIds mismatch"),
+            @ApiResponse(responseCode = "404", description = "Product Not Found to be updated")
+    })
     public Mono<ResponseEntity<Product>> updateProduct(@RequestBody @Valid Product productRequest, @PathVariable(name = "id") String productId) {
         if (!productId.equalsIgnoreCase(productRequest.getId())) {
             throw new IllegalArgumentException("ProductId in path does not match Id in request body");
@@ -35,13 +44,19 @@ public class ProductController {
     }
 
     @PostMapping("/addProduct")
+    @Operation(summary = "Add product to database. Admin access required.")
     public ResponseEntity<Flux<Product>> addProducts(@RequestBody List<@Valid Product> products) {
 
         Flux<Product> productFlux = productService.addProducts(products);
-            return ResponseEntity.status(HttpStatus.CREATED).body(productFlux);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productFlux);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get product by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product found"),
+            @ApiResponse(responseCode = "404", description = "Product Not Found")
+    })
     public Mono<ResponseEntity<Product>> getProduct(@PathVariable String id) {
         return productService.getProductById(id)
                 .map(ResponseEntity.ok()::body);
